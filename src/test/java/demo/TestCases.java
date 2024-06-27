61127c10c0b61c3cd36c9ce70ffe19302fcfc96a
Saraswati Goravanakoll <saraswati.goravanakoll@gmail.com>
2024-06-27 14:14:09 +0530

    ME_QA_XYOUTUBE_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,15 +1,18 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -17,51 +20,187 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.logging.Level;
 
 import demo.utils.ExcelDataProvider;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
-public class TestCases extends ExcelDataProvider{ // Lets us read the data
-        ChromeDriver driver;
+public class TestCases extends ExcelDataProvider { // Lets us read the data
+    ChromeDriver driver;
+    //Wrappers wrappers = new Wrappers();
 
-        /*
-         * TODO: Write your tests here with testng @Test annotation.
-         * Follow `testCase01` `testCase02`... format or what is provided in
-         * instructions
-         */
+    /*
+     * TODO: Write your tests here with testng @Test annotation.
+     * Follow `testCase01` `testCase02`... format or what is provided in
+     * instructions
+     */
+    @Test
+    public void testCase01() {
+        Boolean status;
+        Wrappers.logStatus("Start TestCase", "Test Case 1:", "DONE");
+        driver.get("https://www.youtube.com/");
+        status = driver.getCurrentUrl().equalsIgnoreCase("https://www.youtube.com/");
+        Assert.assertTrue(status, "Landing on You tube home page failed");
 
-        /*
-         * Do not change the provided methods unless necessary, they will help in
-         * automation and assessment
-         */
-        @BeforeTest
-        public void startBrowser() {
-                System.setProperty("java.util.logging.config.file", "logging.properties");
+        //Click on Hamburger menu
+        Wrappers.clickAction(driver, By.xpath("//button[@aria-label='Guide']"));
+        //WebElement guideMenu = driver.findElement(By.xpath("//button[@aria-label='Guide']"));
 
-                // NOT NEEDED FOR SELENIUM MANAGER
-                // WebDriverManager.chromedriver().timeout(30).setup();
+        //Click on about
+        Wrappers.clickAction(driver, By.xpath("//a[text()='About']"));
+        status = Wrappers.waitForPageLoad(driver, "about");
+        Assert.assertTrue(status, "Landing on about page failed");
 
-                ChromeOptions options = new ChromeOptions();
-                LoggingPreferences logs = new LoggingPreferences();
+        //Get message displayed on about page
+        String message = Wrappers.getElemetText(driver, By.xpath("//section[@class='ytabout__content']"));
+        System.out.println("About page message : " + message);
 
-                logs.enable(LogType.BROWSER, Level.ALL);
-                logs.enable(LogType.DRIVER, Level.ALL);
-                options.setCapability("goog:loggingPrefs", logs);
-                options.addArguments("--remote-allow-origins=*");
+        Wrappers.logStatus("End TestCase", "Test Case 1:", status ? "PASS" : "FAIL");
+    }
 
-                System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
+    @Test
+    public void testCase02() throws InterruptedException {
+        Boolean status;
+        Wrappers.logStatus("Start TestCase", "Test Case 2:", "DONE");
+        driver.get("https://www.youtube.com/");
+        status = driver.getCurrentUrl().equalsIgnoreCase("https://www.youtube.com/");
+        Assert.assertTrue(status, "Landing on You tube home page failed");
 
-                driver = new ChromeDriver(options);
+        //Click on Hamburger menu
+        Wrappers.clickAction(driver, By.xpath("//button[@aria-label='Guide']"));
+        // WebElement guideMenu = driver.findElement(By.xpath("//button[@aria-label='Guide']"));
 
-                driver.manage().window().maximize();
+        Wrappers.clickAction(driver, By.xpath("//a[@title='Movies']"));
+        //Thread.sleep(2000);
+
+        Wrappers.scrollToRightExtreem(driver,
+                By.xpath("//span[text()='Top selling']/ancestor::div[@id='title-container']/../following-sibling::div//div[@id='right-arrow']//button"));
+
+        String movieType = Wrappers.getLastElemetTextInList(driver,By.xpath
+                ("//span[text()='Top selling']/ancestor::div[@id='title-container']/../following-sibling::div//span[contains(@class,'grid-movie-renderer-metadata ')]"));
+        SoftAssert sa= new SoftAssert();
+        sa.assertTrue(movieType.contains("Comedy") || movieType.equals("Animation"), "Movie is neither 'Comedy' nor 'Animation'");
+
+        String movieCertification = Wrappers.getLastElemetTextInList(driver,By.xpath
+                ("//span[text()='Top selling']/ancestor::div[@id='title-container']/../following-sibling::div//p[not(text()='Buy or rent') and not(text()='Rent')]"));
+        sa.assertEquals(movieCertification, "A", "Movie is not marked 'A' for Mature");
+
+    }
+
+    @Test
+    public void testCase03() throws InterruptedException {
+        Boolean status;
+        Wrappers.logStatus("Start TestCase", "Test Case 1:", "DONE");
+        driver.get("https://www.youtube.com/");
+        status = driver.getCurrentUrl().equalsIgnoreCase("https://www.youtube.com/");
+        Assert.assertTrue(status, "Landing on You tube home page failed");
+
+        //Click on Hamburger menu
+        Wrappers.clickAction(driver, By.xpath("//button[@aria-label='Guide']"));
+
+        //Click on Music button/link text
+        Wrappers.clickAction(driver,By.xpath("//a[@title='Music']"));
+
+        String title = Wrappers.getElemetText(driver,By.xpath
+                ("//span[(@id='title') and (@class='style-scope ytd-shelf-renderer')]"));
+
+        Wrappers.scrollToRightExtreem(driver,By.xpath
+                ("//span[text()=\""+title+"\"]/ancestor::div[@id='title-container']/../following-sibling::div[@id='contents']//div[@id='right-arrow']//button"));
+
+        String totalTracks = Wrappers.getLastElemetTextInList(driver,By.xpath(
+                "//span[text()=\""+title+"\"]/ancestor::div[@id='title-container']/../following-sibling::div[@id='contents']//p[@id='video-count-text']"));
+
+        String[] splitTotalTracks = totalTracks.split(" ");
+
+        SoftAssert sa = new SoftAssert();
+        sa.assertTrue(Wrappers.convertToNumericValue(splitTotalTracks[0])<=50,"Tracks are more than 50");
+    }
+
+    @Test
+    public void testCase04(){
+        Boolean status;
+        Wrappers.logStatus("Start TestCase", "Test Case 1:", "DONE");
+        driver.get("https://www.youtube.com/");
+        status = driver.getCurrentUrl().equalsIgnoreCase("https://www.youtube.com/");
+        Assert.assertTrue(status, "Landing on You tube home page failed");
+
+        //Click on Hamburger menu
+        Wrappers.clickAction(driver, By.xpath("//button[@aria-label='Guide']"));
+
+        //Click on News button/link text
+        Wrappers.clickAction(driver,By.xpath("//a[@title='News']"));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement latestNewTab = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text()='Latest news posts']/ancestor::div[@id='rich-shelf-header-container']/following-sibling::div[@id='contents']")));
+
+        List<WebElement> latestNews = latestNewTab.findElements(By.xpath(".//div[(@id='content') and (@class='style-scope ytd-rich-item-renderer')]"));
+        if(latestNews.size()<=3){
+            int numberOfVotes =0;
+            for(int i=0;i<latestNews.size();i++){
+                System.out.println("News header"+ i +": "+ Wrappers.getElementFromParentElementAndPrintText(driver,latestNewTab,By.xpath(".//div[(@id='header') ]"),i));
+                System.out.println("News Body"+ i +": "+ Wrappers.getElementFromParentElementAndPrintText(driver,latestNewTab,By.xpath(".//div[(@id='body') ]"),i));
+                try{
+                    String res = Wrappers.getElementFromParentElementAndPrintText(driver,latestNewTab, By.xpath(".//span[@id='vote-count-middle']"), i);
+                    numberOfVotes += Wrappers.convertToNumericValue(res);
+                }
+                catch(NoSuchElementException e){
+                    System.out.println("Vote not present - "+e.getMessage());
+                }
+            }
+            System.out.println(numberOfVotes);
+        }else {
+            int numberOfVotes =0;
+            for(int i=0;i<3;i++){
+                System.out.println("News header"+ i +": "+ Wrappers.getElementFromParentElementAndPrintText(driver,latestNewTab,By.xpath(".//div[(@id='header') ]"),i));
+                System.out.println("News Body"+ i +": "+ Wrappers.getElementFromParentElementAndPrintText(driver,latestNewTab,By.xpath(".//div[(@id='body') ]"),i));
+                try{
+                    String res = Wrappers.getElementFromParentElementAndPrintText(driver,latestNewTab, By.xpath(".//span[@id='vote-count-middle']"), i);
+                    numberOfVotes += Wrappers.convertToNumericValue(res);
+                }
+                catch(NoSuchElementException e){
+                    System.out.println("Vote not present - "+e.getMessage());
+                }
+            }
+            System.out.println("Total likes count of top 3 latest news: "+numberOfVotes);
         }
+        Wrappers.logStatus("End TestCase", "Test Case 4:", "DONE");
+    }
 
-        @AfterTest
-        public void endTest() {
-                driver.close();
-                driver.quit();
 
-        }
+    /*
+     * Do not change the provided methods unless necessary, they will help in
+     * automation and assessment
+     */
+    @BeforeTest
+    public void startBrowser() {
+        System.setProperty("java.util.logging.config.file", "logging.properties");
+
+        // NOT NEEDED FOR SELENIUM MANAGER
+        // WebDriverManager.chromedriver().timeout(30).setup();
+
+        ChromeOptions options = new ChromeOptions();
+        LoggingPreferences logs = new LoggingPreferences();
+
+        logs.enable(LogType.BROWSER, Level.ALL);
+        logs.enable(LogType.DRIVER, Level.ALL);
+        options.setCapability("goog:loggingPrefs", logs);
+        options.addArguments("--remote-allow-origins=*");
+
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
+
+        driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterTest
+    public void endTest() {
+        driver.close();
+        driver.quit();
+
+    }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -8,10 +8,127 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
 import java.time.Duration;
+import java.util.List;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    public static void logStatus(String type, String message, String status) {
+
+        System.out.println(String.format("%s |  %s  |  %s | %s",
+                String.valueOf(java.time.LocalDateTime.now()), type, message, status));
+    }
+
+    public static void clickAction(WebDriver driver, By locator) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+            WebElement buttonElement = wait.until(ExpectedConditions.elementToBeClickable(locator));
+            getElementInView(driver, buttonElement);
+            buttonElement.click();
+        } catch (Exception e) {
+            System.out.println("Exception Occured! " + e.getMessage());
+        }
+    }
+
+    public static void getElementInView(WebDriver driver, WebElement element) {
+        // WebElement element = driver.findElement(locator);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    public static Boolean waitForPageLoad(WebDriver driver, String title) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        Boolean status = wait.until(ExpectedConditions.urlContains(title));
+        return status;
+    }
+
+    public static String getElemetText(WebDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        String text = element.getText().trim();
+        return text;
+    }
+
+    public static String getLastElemetTextInList(WebDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+        List<WebElement> list = driver.findElements(locator);
+        String text = list.get(list.size()-1).getText().trim();
+        return text;
+    }
+
+
+    public static void scrollToRightExtreem(WebDriver driver, By rightClickElement) throws InterruptedException {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement scrollRightButton = wait.until(ExpectedConditions.elementToBeClickable(rightClickElement));
+            //WebElement scrollRightButton = driver.findElement(rightClickElement);
+
+            // Scroll by clicking the button until we reach the right extreme
+            while (true) {
+                // Click the scroll right button
+                scrollRightButton.click();
+                boolean status = scrollRightButton.isDisplayed();
+                if (status) {
+                    continue;
+                }
+
+                // Wait for a short period to allow the scroll to happen
+                Thread.sleep((new java.util.Random().nextInt(3) + 2) * 1000); // Adjust the sleep time as needed
+
+                // Check if at the right extreme
+                if (!status) {
+                    System.out.println("Reached the right extreme of the scroll bar");
+                    break;
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getElementFromParentElementAndPrintText(WebDriver driver,WebElement parentElement, By locator,int elementNo){
+        WebElement element = parentElement.findElements(locator).get(elementNo);
+        String txt = element.getText();
+        return txt;
+    }
+
+    public static long convertToNumericValue(String value) {
+        // Trim the string to remove any leading or trailing spaces
+        value = value.trim().toUpperCase();
+
+        // Check if the last character is non-numeric and determine the multiplier
+        char lastChar = value.charAt(value.length() - 1);
+        int multiplier = 1;
+        switch (lastChar) {
+            case 'K':
+                multiplier = 1000;
+                break;
+            case 'M':
+                multiplier = 1000000;
+                break;
+            case 'B':
+                multiplier = 1000000000;
+                break;
+            default:
+                // If the last character is numeric, parse the entire string
+                if (Character.isDigit(lastChar)) {
+                    return Long.parseLong(value);
+                }
+                throw new IllegalArgumentException("Invalid format: " + value);
+        }
+
+        // Extract the numeric part before the last character
+        String numericPart = value.substring(0, value.length() - 1);
+        double number = Double.parseDouble(numericPart);
+
+        // Calculate the final value
+        return (long) (number * multiplier);
+    }
+
+
+
 }
